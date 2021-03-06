5d207193f009a00df71ebfff7d59752d7b8be474
Monika <monika.machula94@gmail.com>
2021-03-06 09:53:14 +0100

    add useRef exercise

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,28 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+    const refContainer = useRef(null);
+    const divContainer = useRef(null);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(refContainer.current.value)
+        console.log(divContainer.current)
+    }
+useEffect(() => {
+    console.log(refContainer.current)
+    refContainer.current.focus();
+})
+  return (
+      <>
+        <form className='form' onSubmit={handleSubmit}>
+          <div>
+            <input ref={refContainer} type='text'/>
+            <button type='submit'>submit</button>
+          </div>
+        </form>
+          <div ref={divContainer}>Hello World</div>
+      </>
+  )
 };
 
 export default UseRefBasics;
